c62d1d4832c4eb15523034845221befe2fb96e75
“ElvinShalmiev” <elvin080192@gmail.com.com>
2023-02-02 12:18:41 +0200

    added "cors"

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,23 @@ import { notFound } from "./middleware/not-found.js";
 import { booksRouter } from "./routes/books.js";
 import { cardsRouter } from "./routes/cards.js";
 import { studentsRouter } from "./routes/students.js";
+import cors from "cors";
 
 const app = express();
 
 /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 //middlewares:
 
-app.use((req,res,next)=>{
+app.use(cors({origin:'http://localhost:3000'}))
+
+
+/* app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","*");
    res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type,x-requested-with");
     next();
-})
+}) */
 
 app.use(express.json());
 app.use(morgan("dev"));
---
+++ src/middleware/accessHeader.ts
@@ -0,0 +1,6 @@
+/* app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*");
+   res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type,x-requested-with");
+    next(); */
