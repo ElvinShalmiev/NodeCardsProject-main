a7c1dbdff45adf508bdf63bb25cb8b26e58e045c
“ElvinShalmiev” <elvin080192@gmail.com.com>
2023-02-07 12:00:27 +0200

    JWT,Roles, populate()

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,6 +7,8 @@ import bcrypt from "bcryptjs";
 import { validateSignIn } from "../middleware/verifySignInBody.js";
 import { Role } from "../db/models/role.js";
 const router = Router();
+import jwt from "jsonwebtoken";
+import authConfig from "../db/config/auth.config.js";
 
 //api/auth/signup
 router.post("/signup", validateSignUp, userAlreadyExists, async (req, res) => {
@@ -29,8 +31,11 @@ router.post("/signup", validateSignUp, userAlreadyExists, async (req, res) => {
 router.post("/signin", validateSignIn, async (req, res) => {
   //email and password:
   try {
-    const user = await User.findOne({ email: req.body.email });
 
+    //SELECT * FROM user JOIN Roles ON ...
+    const user = await User.findOne({ email: req.body.email }).populate<{
+      roles: Array<typeof Role>;
+    }>("roles");
     if (!user) {
       return res.status(401).json({ message: "No Such User" });
     }
@@ -46,8 +51,22 @@ router.post("/signin", validateSignIn, async (req, res) => {
       return res.status(401).json({ message: "Invalid Credentials" });
     }
 
-    return res.status(200).json({ message: "Sign in succesfull" });
-  } catch (e) {}
+    const token = jwt.sign({ id: user.id }, authConfig.secret, {
+      expiresIn: "30d",
+    });
+
+    const authorities = [];
+    for (let i = 0; i < user.roles.length; i++) {
+      authorities.push(`ROLE_` + user.roles[i].name.toUpperCase());
+    }
+    return res.status(200).json({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+      accessToken: token,
+    });
+  } catch (e) {return res.status(500).json({ message: "Server Error", error: e });}
 });
 
 export { router as authRouter };
